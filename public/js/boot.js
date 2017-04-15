1eece653b1a5d1186a1c3233ba800452355bc595
Marcin Krawczyk <mkrawczyk.ixara@gmail.com>
2017-04-15 08:38:16 +0200

    improved multi touch

--- public/js/boot.js
+++ public/js/boot.js
@@ -1,27 +1,44 @@
 import Renderer from 'core/renderer/renderer';
 import Rectangle from 'core/shape/rectangle';
 import Circle from 'core/shape/circle';
+import ScratchCard from 'scratch/scartchcard';
+
 import Selection from 'selection';
 
 const RENDERER_ID = 'renderer';
 const SELECTION_ID = 'selection';
 
-var renderer = new Renderer(document.getElementById(RENDERER_ID));
-var selection = new Selection(document.getElementById(SELECTION_ID));
+//var renderer = new Renderer(document.getElementById(RENDERER_ID));
+//var selection = new Selection(document.getElementById(SELECTION_ID));
 
-renderer.add(new Rectangle({
-    width: 100,
-    height: 100
-}));
-renderer.add(new Circle({
-    x: 100,
-    y: 100
-}));
-renderer.add(new Circle({
-    x: 100,
-    y: 100
-}));
-renderer.renderAll();
+var options = {
+    id: 'canvas',
+    brushSize: 50,
+    lineJoin: 'round',
+    percentRequired: 80,
+    fillColor: 'rgb(100, 100, 13)'
+};
+
+var scratch = new ScratchCard(options);
+
+
+scratch.addEventListener('success', function (e) {
+    console.info('You can do whatever you want here!');
+}, false);
+// renderer.add(new Rectangle({
+//     width: 100,
+//     height: 100
+// }));
+// renderer.add(new Circle({
+//     x: 100,
+//     y: 100
+// }));
+// renderer.add(new Circle({
+//     x: 100,
+//     y: 100
+// }));
+//
+// renderer.renderAll();
 
 
 
